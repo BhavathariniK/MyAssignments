18ea8d6f89869e64fc7ec313220bef9ebce2ffe2
BhavathariniK <122194674+BhavathariniK@users.noreply.github.com>
2023-01-08 11:01:36 +0530

    Add files via upload
    Day 1 asssigments

---
+++ day1/IsPrime.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 30;
+		for (int i = 2; i < n - 1; i++) {
+			if (n % i == 0) {
+				System.out.println("Prime");
+			} else {
+
+				System.out.println("NotPrime");
+			}
+		}
+	}
+
+}
---
+++ day1/Mobile.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int ramSize = 32;
+		boolean isDamaged = false;
+		String modelName = "RealMe";
+		
+		System.out.println("RAM Size:" + ramSize);
+		System.out.println(isDamaged);
+		System.out.println(modelName);
+		
+			
+
+	}
+
+}
